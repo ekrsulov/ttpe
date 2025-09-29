614e6aaccb23d33c6acebe194d9d55fb59aa493a
Ernesto Krsulovic <ernesto.krsulovic@gmail.com>
2025-09-29 15:59:26 -0300

    refactor: Consolidate overlay imports in Canvas and index files

--- src/components/overlays/index.ts
+++ src/components/overlays/index.ts
@@ -4,3 +4,4 @@ export { SubpathOverlay } from './SubpathOverlay';
 export { ShapePreview } from './ShapePreview';
 export { OpticalAlignmentOverlay } from './OpticalAlignmentOverlay';
 export { FeedbackOverlay } from './FeedbackOverlay';
+export { TransformationOverlay } from './TransformationOverlay';
